3311bbc4ee267bb4e60cb9edbcd2b264a208481c
guisandroni <gui.sandroni@gmail.com>
2025-08-27 10:53:09 -0300

    feat: config conection with data base bear my sql

--- Resources/DbRepository.cs
+++ Resources/DbRepository.cs
@@ -6,13 +6,42 @@ namespace minimal_api.Resources
     public class DbRepository : DbContext
     {
 
+        private readonly IConfiguration _configuration;
+        public DbRepository(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
         public DbSet<AdminEntity> Admins { get; set; } = default!;
+
+        public DbSet<VehicleEntity> Vehicles { get; set; } = default!;
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<AdminEntity>().HasData(
+                new AdminEntity
+                {
+                    Id = 1,
+                    Email = "Alexandre@admin.com",
+                    Password = "admin123",
+                    Perfil = "Admin"
+                }
+            );
+        }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-           optionsBuilder.UseMySql("string de conexao",ServerVersion.AutoDetect("string de conexao"));
+
+            if (!optionsBuilder.IsConfigured)
+            {
 
 
+                var connectionString = _configuration.GetConnectionString("mysql")?.ToString();
+                if (!string.IsNullOrEmpty(connectionString))
+                    optionsBuilder.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString));
+                else
+                    optionsBuilder.UseMySql("Server=localhost;Database=minimal_api;User=root;Password=root;", ServerVersion.AutoDetect("Server=localhost;Database=minimal_api;User=root;Password=root;"));
+
+            }
         }
     }
 }
