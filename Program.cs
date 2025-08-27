9305b6dcf21b8868e40872b53e98fc0455ca7f94
guisandroni <gui.sandroni@gmail.com>
2025-08-27 10:55:05 -0300

    fix: logic admin service endpoit post, add scoped adminInterfaces and services

--- Program.cs
+++ Program.cs
@@ -1,19 +1,33 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using minimal_api.Model.DTO;
+using minimal_api.Model.Interface;
+using minimal_api.Model.Service;
+using minimal_api.Resources;
 
 var builder = WebApplication.CreateBuilder(args);
 
+
+builder.Services.AddScoped<AdminInterfaceService, AdminService>();
 // Add services to the container.
+
+builder.Services.AddDbContext<DbRepository>(options =>
+{
+    options.UseMySql(
+        builder.Configuration.GetConnectionString("mysql"),
+        ServerVersion.AutoDetect(builder.Configuration.GetConnectionString("mysql"))
+        );
+});
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 
-
 app.MapGet("/", () => "ccc");
 
-app.MapPost("/login", (LoginDTO loginDto) =>
+app.MapPost("/login", ( [FromBody] LoginDTO loginDto, AdminInterfaceService adminService) =>
 {
-    if (loginDto.Email == "example@teste.com" && loginDto.Password == "password123")
+    if (adminService.Login(loginDto) != null)
     {
         return Results.Ok(new { Message = "Login successful" });
     }
